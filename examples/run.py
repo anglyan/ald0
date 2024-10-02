52b2e2de1f93f6dc1c783186e4c9f782cd3bf3fc
yyardi <yyardi@imsa.edu>
2024-10-02 11:51:24 -0500

    Run changes

--- examples/run.py
+++ examples/run.py
@@ -1,23 +1,71 @@
 from ald0 import aldprocess
 import numpy as np
+import matplotlib.pyplot as pt
+import os
 
+savepath = "/home/yyardi/projects/ald0/"
+
+compound = "TMA200"
 # We define an ALD process based on chemistry
-ald = aldprocess('TMA200', noise=0)
+ald = aldprocess(compound, noise=0)
 
 #An ALD process comprises four times tdose1, tpurge1, tdose2, tpurge2
 
 td1 = np.arange(0, 0.1, 0.002)
+tp1 = np.arange(0, 0.5, 0.002)
 gr_l = []
 
-for t in td1:
-    # The method cycle in ALD0 computes the total thickness per cycle for
-    # N consecutive cycles
-    gr = np.mean(ald.cycle(t,5,1,5, N=4))
-    gr_l.append(gr)
+def dose1():
+    
+    
+    for t in td1:
+        # The method cycle in ALD0 computes the total thickness per cycle for
+        # N consecutive cycles
+        gr = np.mean(ald.cycle(t,0.5,0.1,0.5, N=4))
+        gr_l.append(gr)
+    pt.title(compound + 'Dose 1', fontsize=14, fontweight='bold', color='blue')
+    pt.xlabel('Time') 
+    pt.ylabel('Thickness') # angstrom
+    pt.plot(td1, gr_l)
+    pt.savefig(savepath + compound +'Dose 1.png')
 
-import matplotlib.pyplot as pt
+def dose2():
+    for t in td1:
+        # The method cycle in ALD0 computes the total thickness per cycle for
+        # N consecutive cycles
+        gr = np.mean(ald.cycle(0.1,0.5,t,0.5, N=4))
+        gr_l.append(gr)
+    pt.title(compound + 'Dose 2', fontsize=14, fontweight='bold', color='blue')
+    pt.xlabel('Time')
+    pt.ylabel('Thickness')
+    pt.plot(td1, gr_l)
+    pt.savefig(savepath + compound +'Dose 2.png')
 
-pt.plot(td1, gr_l)
-pt.show()
+
+def purge1():
+    for t in tp1:
+        # The method cycle in ALD0 computes the total thickness per cycle for
+        # N consecutive cycles
+        gr = np.mean(ald.cycle(0.1,t,0.1,0.5, N=4))
+        gr_l.append(gr)
+    pt.title(compound + 'Purge 1', fontsize=14, fontweight='bold', color='blue')
+    pt.xlabel('Time')
+    pt.ylabel('Thickness')
+    pt.plot(tp1, gr_l)
+    pt.savefig(savepath + compound +'Purge 1.png')
+
+def purge2():
+    for t in tp1:
+        # The method cycle in ALD0 computes the total thickness per cycle for
+        # N consecutive cycles
+        gr = np.mean(ald.cycle(0.1,0.5,0.1,t, N=4))
+        gr_l.append(gr)
+    pt.title(compound + 'Purge 2', fontsize=14, fontweight='bold', color='blue')
+    pt.xlabel('Time')
+    pt.ylabel('Thickness')
+    pt.plot(tp1, gr_l)
+    pt.savefig(savepath + compound +'Purge 2.png')
+
+purge2()
 
 
